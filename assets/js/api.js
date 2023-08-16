f654d4f2a4ba75a4cf5c8ae9230877eef2e95b65
DaianePorcena <daianeporcena7@gmail.com>
2023-08-16 18:09:19 -0300

    Adição das softskills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,6 @@
 
 async function fetchProfileData() {
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/DaianePorcena/Portfolio_dio/main/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
